e8f8e3b4f7b80fce1f4b86137bd02b5aca98c6c3
Yuchen_He <Yuchen_He@n.t.rd.honda.co.jp>
2020-02-27 19:10:49 +0900

    [modify]cv6_project1_fix

--- 006_course_documents/computer_vision/week3_4_5/week4/project_1/object_tracking.py
+++ 006_course_documents/computer_vision/week3_4_5/week4/project_1/object_tracking.py
@@ -4,22 +4,26 @@ import numpy as np
 
 
 class Object_tracking(object):
-    def __init__(self, tolerate):
+    def __init__(self, ransac_tolerate, bbox_color, bbox_thickness):
         self.sift = cv2.xfeatures2d.SURF_create()
 
         # parameters for draw bbox
-        self.box_color = (0, 255, 255)
-        self.box_thickness = 3
+        self.bbox_color = bbox_color
+        self.bbox_thickness = bbox_thickness
+
+        # parameter for flann
+        self.FLANN_INDEX_KDTREE = 0
+        self.index_params  = dict(algorithm = self.FLANN_INDEX_KDTREE, trees = 5)
+        self.search_params = dict(checks = 50)
+        self.flann = cv2.FlannBasedMatcher(self.index_params, self.search_params)
 
         # parameters for ransac
-        self.tolerate = tolerate
+        self.tolerate = ransac_tolerate
 
     def draw_bbox(self, img, bbox):
-        # draw a bbox on to a image (in BGR format)
+        # Draw a bbox on to a image (in BGR format)
         # return: image with bbox
-        #img_bbox = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 255), 5)
-        #print('first_frame: ', img.shape)
-        img_bbox = cv2.polylines(img, [np.int32(bbox)], True, self.box_color, self.box_thickness, cv2.LINE_AA)
+        img_bbox = cv2.polylines(img, [np.int32(bbox)], True, self.bbox_color, self.bbox_thickness, cv2.LINE_AA)
         return img_bbox
 
 
@@ -37,72 +41,58 @@ class Object_tracking(object):
         frame_with_bbox = self.draw_bbox(frame, bbox_orig)
     
         # Detect the keypoints and return them
-        #import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         lt, rt, lb, rb = bbox_orig.reshape(-1, 2)
-        frame_crop = frame[lt[0]:rt[0], lt[1]:lb[1]]
+        shift = self.bbox_thickness
+        frame_crop = frame[(lt[1]+shift):(lb[1]-shift), (lt[0]+shift):(rt[0]-shift)]
+        # cv2.imwrite('frame_crop_1st.jpg', frame_crop)
         #print('frame_crop: ',frame_crop.shape)
         
         kp_orig, des_orig = self.sift.detectAndCompute(frame_crop, None)
-        return frame_with_bbox, kp_orig, des_orig
+        return frame_with_bbox, frame_crop, kp_orig, des_orig
 
 
-    def track_detect(self, frame, kp_orig, des_orig, bbox_orig):
+    def track_detect(self, frame, frame_crop_first, kp_orig, des_orig):
         # Detect keypoints
         kp, des = self.sift.detectAndCompute(frame, None)
     
         # Do matching with the previous frame's keypoints
-        bf = cv2.BFMatcher()
-        matches = bf.knnMatch(des_orig, des, k=2)     # or using DescriptorMatcher
+        matches = self.flann.knnMatch(des_orig,des,k=2)
+        # bf = cv2.BFMatcher()
+        # matches = bf.knnMatch(des_orig, des, k=2)
     
-        # Get the matched points
-        match_points_pre  = []
-        match_points_this = []
-    #    for m, n in matches:
-    #        img1_idx = m.queryIdx
-    #        img2_idx = m.trainIdx
-    #        (x1, y1) = kp_orig[img1_idx].pt
-    #        (x2, y2) = kp[img2_idx].pt
-    #        match_points_pre.append((int(x1), int(y1)))
-    #        match_points_this.append((int(x2), int(y2)))
-
         # Select good points, and get the (x,y) in the img
         good = []
         for m, n in matches:
-            if m.distance < 0.75 * n.distance:
+            if m.distance < 0.5 * n.distance:
                 good.append([m])
         good.sort(key=lambda x: x[0].distance)
 
-        for match in good:
-            img1_idx = match[0].queryIdx
-            img2_idx = match[0].trainIdx
-            (x1, y1) = kp_orig[img1_idx].pt
-            (x2, y2) = kp[img2_idx].pt
-            match_points_pre.append((int(x1), int(y1)))
-            match_points_this.append((int(x2), int(y2)))
+        # Get the matched points
+        # match_points_pre  = []
+        # match_points_this = []
+        # for match in good:
+        #     img1_idx = match[0].queryIdx
+        #     img2_idx = match[0].trainIdx
+        #     (x1, y1) = kp_orig[img1_idx].pt
+        #     (x2, y2) = kp[img2_idx].pt
+        #     match_points_pre.append((int(x1), int(y1)))
+        #     match_points_this.append((int(x2), int(y2)))
+
+        match_points_src = np.float32([ kp_orig[m[0].queryIdx].pt for m in good ]).reshape(-1,1,2)
+        match_points_dst = np.float32([ kp[m[0].trainIdx].pt for m in good ]).reshape(-1,1,2)
 
         # RANSAC to refine the matched points
-        # S is a mask of 1/0 represents matched or mismatched
-        M, S = self.ransac_match(match_points_pre, match_points_this)
-    
-        #x_min, y_min, x_max, y_max = 1000, 1000, 0, 0 
-        #S_list = S.ravel().tolist()
-        #match_final = []
-        #for i in range(len(S_list)):
-        #    if S_list[i] == 1:
-        #        match_final.append(match_points_this[i])
-                #if match_points_this[i][0] < x_min:
-                #    x_min = match_points_this[i][0]
-                #if match_points_this[i][1] < y_min:
-                #    y_min = match_points_this[i][1]
-                #if match_points_this[i][0] > x_max:
-                #    x_max = match_points_this[i][0]
-                #if match_points_this[i][1] > y_max:
-                #    y_max = match_points_this[i][1]
+        # mask is a mask of 1/0 represents matched or mismatched
+        # M, mask = self.ransac_match(match_points_src, match_points_dst)
+        M, mask = cv2.findHomography(match_points_src, match_points_dst, cv2.RANSAC, self.tolerate)
+        matchesMask = mask.ravel().tolist()
 
-        #bbox_tracked = [x_min, y_min, x_max, y_max]
-        # Perspect the original bbox
-        # bbox_tracked = cv2.warpPerspective(bbox_pre, M)
-        bbox_tracked = cv2.perspectiveTransform(np.float32(bbox_orig), M)
+        # Perspective transform from the original bbox
+        # import pdb; pdb.set_trace()
+        h,w = frame_crop_first.shape[0], frame_crop_first.shape[1]
+        bbox_first = np.float32([[1,1], [w-1,1], [1,h-1], [w-1,h-1]]).reshape(-1,1,2)
+        bbox_tracked = cv2.perspectiveTransform(bbox_first, M)
         return bbox_tracked
 
 
@@ -113,29 +103,28 @@ def main(video_input_path, bbox_orig, video_output_path):
     out = cv2.VideoWriter(video_output_path, fourcc, 30.0, (1280, 720))
 
     count = 1
-    obj_track = Object_tracking(tolerate=5)
+    obj_track = Object_tracking(ransac_tolerate=3, bbox_color=(0, 255, 255), bbox_thickness=3)
+    print('Generating tracked video to {}'.format(video_output_path))
     while(True):
         ret, frame = video_input.read()
-        #import pdb; pdb.set_trace()
         if ret:
             #import pdb; pdb.set_trace()
             if count == 1:
-                #import pdb; pdb.set_trace()
-                frame_first, kp_orig, des_orig = obj_track.detect_first_frame(frame, bbox_orig)
-                print('i: {0}, bbox: {1}'.format(count, bbox_orig))
-                cv2.imshow("Traking Object", frame_first)
-                cv2.waitKey(25)
+                frame_first, frame_crop_first, kp_orig, des_orig = obj_track.detect_first_frame(frame, bbox_orig)
+                # print('i: {0}, bbox: {1}'.format(count, bbox_orig))
+                # cv2.imshow("Traking Object", frame_first)
+                # cv2.waitKey(25)
                 out.write(frame_first)
                 count += 1
             
             else:
-                bbox_tracked = obj_track.track_detect(frame, kp_orig, des_orig, bbox_orig)
-                frame_tracked = obj_track.draw_bbox(frame, bbox_tracked)   # change bbox shape
+                bbox_tracked  = obj_track.track_detect(frame, frame_crop_first, kp_orig, des_orig)
+                frame_tracked = obj_track.draw_bbox(frame, bbox_tracked)
                 out.write(frame_tracked)
-                print('i: {0}, bbox: {1}'.format(count, bbox_tracked))
+                # print('i: {0}, bbox: {1}'.format(count, bbox_tracked))
+                # cv2.imshow('Traking Object',frame_tracked)
+                # cv2.waitKey(25)
                 count += 1
-                cv2.imshow('Traking Object',frame_tracked)
-                cv2.waitKey(25)
         else:
              break
 
@@ -145,8 +134,11 @@ def main(video_input_path, bbox_orig, video_output_path):
 
 
 if __name__ == '__main__':
-    video_input_path  = "./arduino2.mp4"
+    video_input_path  = "./arduino.mp4"
     video_output_path = "./tracked_arduino.mp4"
-    bbox_orig = np.array([[355, 181], [844, 181], [844, 530], [355, 530]]).reshape(-1,1,2)
-    #bbox_orig = np.array([[187, 350], [540, 350], [540, 846], [187, 846]]).reshape(-1,1,2)
+    bbox_orig = np.array([[355, 181], [844, 181], [844, 530], [355, 530]]).reshape(-1,1,2)       # 1280*720
     main(video_input_path, bbox_orig, video_output_path)
+
+    # It will take several minutes to generate video
+    # TODO: Resize the image to be smaller
+    print('Video has been saved to {}'.format(video_output_path))
